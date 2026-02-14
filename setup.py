7a7fa57df88cd277260df3d2c87c40109e8eea63
BRUTALLOLOL <114761231+BRUTALLOLOL@users.noreply.github.com>
2026-02-14 23:28:14 +0500

    1.0.0

--- setup.py
+++ setup.py
@@ -1,33 +1,37 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Extension
+import pybind11
+import numpy as np
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# C++ module for fast Volterra solver
+cpp_module = Extension(
+    'kernel_experience._solvers_cpp',
+    sources=['src/kernel_experience/solvers.cpp'],
+    include_dirs=[pybind11.get_include(), np.get_include()],
+    language='c++',
+    extra_compile_args=['-O3', '-std=c++11'],
+)
+
 setup(
     name="kernel-experience-tools",
-    version="0.2.0",
+    version="1.0.0",  # 🚀 First stable release with C++ backend
     author="Artem Vozmishchev",
     author_email="xbrutallololx@gmail.com",
     description="Library for projecting memory kernels to experience functions",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BRUTALLOLOL/kernel-experience-tools",
-    
-    # Package structure
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    
-    # Python compatibility
     python_requires=">=3.7",
-    
-    # Dependencies - pip will handle version compatibility
     install_requires=[
         "numpy",
         "scipy>=1.6.0",
         "matplotlib>=3.3.0",
     ],
-    
-    # PyPI metadata
+    ext_modules=[cpp_module],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
@@ -43,10 +47,6 @@ setup(
         "Topic :: Scientific/Engineering :: Mathematics",
         "Topic :: Scientific/Engineering :: Physics",
     ],
-    
-    # Build options
     include_package_data=True,
     zip_safe=False,
 )
-
-
