4ca218ae5041f87dcf80db62455e723dbe58442e
BRUTALLOLOL <114761231+BRUTALLOLOL@users.noreply.github.com>
2026-02-15 13:12:59 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,10 +25,10 @@ else:
     else:
         compile_args.append('-std=c++11')
 
-# C++ module for Volterra solver
+# C++ module for Volterra solver (stay with _solvers_cpp)
 solver_module = Pybind11Extension(
-    'kernel_experience._solver_cpp',  # обрати внимание: solver, не solvers
-    sources=['src/kernel_experience/solver.cpp'],
+    'kernel_experience._solvers_cpp', 
+    sources=['src/kernel_experience/solvers.cpp'],
     include_dirs=[np.get_include()],
     language='c++',
     extra_compile_args=compile_args,
@@ -63,7 +63,7 @@ setup(
         "scipy>=1.6.0",
         "matplotlib>=3.3.0",
     ],
-    ext_modules=[solver_module, projection_module],  # два модуля!
+    ext_modules=[solver_module, projection_module], 
     cmdclass={"build_ext": build_ext},
     classifiers=[
         "Programming Language :: Python :: 3",
